f9227a3ba9e35ab1e3790da8cc3dd61be5ca72f2
Michael Craun <michael.craun@gmail.com>
2020-12-02 09:33:08 -0500

    Finished Day 2 - Password Philosophy

---
+++ PasswordPhilosophy.playground/Contents.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+typealias Data = (parameter: String, value: String)
+
+func parse() -> [Data] {
+    let report = Bundle.main.path(forResource: "Passwords", ofType: "txt")!
+    let content = try! String(contentsOfFile: report)
+    let components = content.components(separatedBy: "\n")
+    var dict: [(String, String)] = []
+    components.forEach({
+        let components = $0.components(separatedBy: ": ")
+        dict.append((components[0], components[1]))
+    })
+    return dict
+}
+
+func validPasswordsFromOldCompany(containedIn data: [Data]) -> [Data] {
+    var valid: [Data] = []
+    for (parameter, value) in data {
+        var mutableParameter = parameter
+        
+        // Get identifying character and remove extraneous character
+        let delimiter = mutableParameter.removeLast()
+        mutableParameter.removeLast()
+        
+        // Compile amount of acceptable characters
+        let components = mutableParameter.components(separatedBy: "-")
+        let range = Int(components[0])!...Int(components[1])!
+        
+        // Filter value to obtain all occurences of delimiter within value, then compare count of found delimiters to compiled range
+        let delimiters = value.filter({ $0 == delimiter })
+        if range.contains(delimiters.count) {
+            print(delimiter, delimiters, range)
+            valid.append((parameter: parameter, value: value))
+        }
+    }
+    return valid
+}
+
+func validPasswordsForOTC(containedIn data: [Data]) -> [Data] {
+    var valid: [Data] = []
+    for (parameter, value) in data {
+        var mutableParameter = parameter
+        
+        // Get identifying character and remove extraneous character
+        let delimiter = mutableParameter.removeLast()
+        mutableParameter.removeLast()
+        
+        // Compile indecies
+        let components = mutableParameter.components(separatedBy: "-")
+        let firstIndex = value.index(value.startIndex, offsetBy: Int(components[0])! - 1)
+        let secondIndex = value.index(value.startIndex, offsetBy: Int(components[1])! - 1)
+        
+        // Compare characters and indecies and append if either case is valid
+        if value[firstIndex] == delimiter && value[secondIndex] != delimiter {
+            valid.append(Data(parameter, value))
+        } else if value[firstIndex] != delimiter && value[secondIndex] == delimiter {
+            valid.append(Data(parameter, value))
+        }
+    }
+    return valid
+}
+
+let data = parse()
+let validPasswordFromOldCompany = validPasswordsFromOldCompany(containedIn: data)
+print(validPasswordFromOldCompany.count)                                                // 467
+let validPasswords = validPasswordsForOTC(containedIn: data)
+print(validPasswords.count)                                                             // 441
